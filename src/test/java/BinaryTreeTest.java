84f31904e65f15c75e786e8723f1e5f59e6536a1
kaushiki <kaushiki.ak88@gmail.com>
2021-03-04 21:31:53 +0530

    ADDED: add elements and search 63 in BST

--- src/test/java/BinaryTreeTest.java
+++ src/test/java/BinaryTreeTest.java
@@ -11,4 +11,24 @@ public class BinaryTreeTest {
         int size = bt.getSize();
         Assert.assertEquals(3, size);
     }
+    @Test
+    public void givenANumberIfPresentInBinaryTreeShouldReturnTrue() {
+        BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.add(56);
+        bt.add(30);
+        bt.add(70);
+        bt.add(22);
+        bt.add(40);
+        bt.add(61);
+        bt.add(94);
+        bt.add(10);
+        bt.add(64);
+        bt.add(3);
+        bt.add(16);
+        bt.add(63);
+        bt.add(67);
+
+        boolean result = bt.search(63);
+        Assert.assertEquals(true, result);
+    }
 }
